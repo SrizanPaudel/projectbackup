f9ac70ae752c80bbb70ff0c344c59b8c354a4f1c
Prabesh Pandey <prabesh.code@gmail.com>
2025-11-27 14:11:11 +0545

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/dbConnection');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+connectDB();
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
